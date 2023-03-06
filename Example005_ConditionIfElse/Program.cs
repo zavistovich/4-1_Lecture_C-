441682b01f87850e0e4c564dd1e879d7871b11f0
serge <szavistovich@gmail.com>
2023-03-06 23:01:32 +0300

    Example005

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter the user name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Wow! This is Masha");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
